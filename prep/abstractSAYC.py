97a08cd30e669f616fe76e2067f875ea45ac45bf
sinyaw <sinyaw@acm.org>
2025-08-09 22:55:12 -0700

    check

--- prep/abstractSAYC.py
+++ prep/abstractSAYC.py
@@ -12,7 +12,7 @@ import pprint
 import ast
 
 class AbstractSAYC:
-    def __init__(self, fname='baseSAYC.json', seq=[], name=''):
+    def __init__(self, fname='baseSAYC.json'):
         if (os.path.exists(fname) == False):
             raise FileNotFoundError(f"File {fname} does not exist.")
         
@@ -21,23 +21,78 @@ class AbstractSAYC:
         fstr = fstr.replace('\n', '')
         fstr = fstr[fstr.find('{'):-1]
         self.data = json.loads(fstr)
-        self.seq = seq
-        self.name = name
         self.rules = []
+        self.bidQue = []
         return
 
-    def AbsObj(self):
-        rules = [x for x in self.data['BidRules'] if x['BidSeq'] == self.seq]
-        seqObj = {'Ctx': {'Name': self.name, 'Seq': self.seq}, 'Choices': []}
+    def init(self):
+        self.AbsObj(name='Open', seq=[])
+        #self.initQ()
+
+    def initQ(self):
+        self.bidQue = []
+        tQ = []
+        for x in self.rules[0]['Choices']:
+            if (x['Bid'] not in self.bidQue):
+                tQ.append(x['Bid'])
+        for x in tQ:
+            self.bidQue.append([x ,'-'])
+        for x in tQ:
+            self.bidQue.append([x])
+
+    def AbsObj(self, name='AbstractSAYC', seq=[]):
+        rules = [x for x in self.data['BidRules'] if x['BidSeq'] == seq]
+        if (len(rules) == 0):
+            return
+
+        seqObj = {'Ctx': {'Name': name, 'Seq': seq}, 'Choices': []}
         for x in rules:
-            obj = {'Bid': x['Bid']}
+            obj = [y for y in seqObj['Choices'] if x['Bid'] == y['Bid']]
+            isnew = (len(obj) == 0)
+            if isnew:
+                obj = {'Bid': x['Bid']}
+            else:
+                obj = obj[0]
             if 'Criteria' in x:
-                obj['Criteria'] = x['Criteria']
+                if ('Criteria' in obj):
+                    obj['Criteria'].append(x['Criteria'])
+                else:
+                    obj['Criteria'] = [x['Criteria']]
             else:
                 obj['Criteria'] = {}
-            seqObj['Choices'].append(obj)
+            for k in ['Convention', 'GF', 'Forcing']:
+                if k in x:
+                    obj[k] = x[k]
+            if isnew:
+                seqObj['Choices'].append(obj)
+            self.addToQueue(x['Bid'], seq)
         self.rules.append(seqObj)
     
+    def addToQueue(self, bid, seq):
+        toAdd = list(seq)
+        toAdd.append(bid)
+        toAdd2 = list(toAdd)
+        toAdd2.append('-')
+        if (toAdd not in self.bidQue):
+            self.bidQue.append(toAdd)
+        if (toAdd2 not in self.bidQue):
+            self.bidQue.append(toAdd2)
+
+    def loopQ(self):
+        while (len(self.bidQue) > 0):
+            seq = self.bidQue.pop(0)
+            qName = self.makeName(seq)
+            self.AbsObj(qName, seq)
+
+    def makeName(self, seq):
+        str = ''
+        for i in seq:
+            if (i == '-'):
+                str += 'p'
+            else:
+                str += i
+        return str
+
     def output(self):
         p = pprint.PrettyPrinter(indent=2,width=132)
         p.pprint(self.rules)
@@ -46,11 +101,10 @@ if __name__ == "__main__":
     # Example usage
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', type=str, default='baseSAYC.json')
-    parser.add_argument('-q', '--seq', type=str, default='[]')
-    parser.add_argument('-n', '--name', type=str, default='SAYC')
     args = parser.parse_args()
 
-    sayc = AbstractSAYC(args.file, ast.literal_eval(args.seq), args.name)
-    sayc.AbsObj()
+    sayc = AbstractSAYC(args.file)
+    sayc.init()
+    sayc.loopQ()
     sayc.output()
 
