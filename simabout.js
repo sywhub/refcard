68ea2b6880fc222b68a7546c6698db1f04a834d6
sinyaw <sinyaw@acm.org>
2026-05-17 22:31:27 -0700

    Lebensohl after 1NT interference

--- simabout.js
+++ simabout.js
@@ -1,3 +1,6 @@
+enSimAbout = `<h3>About Bid Simulator and Statistics</h3>`;
+zhSimAbout = `<h3>叫牌模擬器和統計資料</h3>`;
+
 function AboutSimulate(d) {
     var e = document.getElementById(d);
     clearContents(e);
@@ -12,4 +15,7 @@ function AboutSimulate(d) {
     feedback.setAttribute('href', 'mailto:syw.cuper+bridge@gmail.com?Subject=Feedback%20for%20Bidding%20Quick%20Reference')
     feedback.innerHTML = trEnZh('Send Feedback');
     left.appendChild(feedback);
+
+    var right = gridElement(divDisp, '', 2, 1);
+    right.innerHTML = Config.OptionItems.Language.IDs[Config.OptionItems.Language.Value] == 'en-US' ? enSimAbout : zhSimAbout;
 }
